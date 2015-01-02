de5687b505ac111da1fb051e0e3ae69852a23bb5
Paul Welter <pwelter@loresoft.com>
2015-01-02 10:08:42 -0600

    update bootstrap.cmd

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,3 +1,5 @@
 @echo off
+Nuget.exe restore "Source\FluentHtml.sln"
+
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
 NuGet.exe install xunit.runners -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
